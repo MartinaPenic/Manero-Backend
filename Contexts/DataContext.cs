73c91f74d3146061a0724af92736b6fc2604df34
G-Brandmaier <brandmaier.gabriella@gmail.com>
2023-05-02 16:38:48 +0200

    User, identity och adress information

--- Contexts/DataContext.cs
+++ Contexts/DataContext.cs
@@ -1,14 +1,17 @@
-﻿
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using WebApi.Models.Entities;
 
-namespace WebApi.Contexts
+namespace WebApi.Contexts;
+
+public class DataContext : IdentityDbContext
 {
-    public class DataContext : IdentityDbContext
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
     {
-        public DataContext(DbContextOptions<DataContext> options) : base(options)
-        {
 
-        }
     }
+
+    public DbSet<UserEntity> UserProfiles { get; set; }
+    public DbSet<AddressEntity> Addresses { get; set; }
+
 }
