57aeae441a86055c451f1cc778c62fb17f44273c
Stefanie Schubert <stefanie.schubert@yh.nackademin.se>
2023-05-04 14:49:37 +0200

    CreditcardEntity

---
+++ Models/Entities/CreditcardEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApi.Models.Entities
+{
+    public class CreditcardEntity
+    {
+        public int Id { get; set; }
+        public string CardNumber { get; set; }
+        public DateTime ExpirationTime { get; set; }
+        public string CVV { get; set; }
+
+    }
+}
