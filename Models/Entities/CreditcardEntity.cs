f63877d97d4ec50824eb1837124e882b6181dfd6
Martina Penic <martinapenic87@gmail.com>
2023-05-06 13:25:23 +0200

    Resolvig merge

--- Models/Entities/CreditcardEntity.cs
+++ Models/Entities/CreditcardEntity.cs
@@ -1,11 +1,29 @@
-﻿namespace WebApi.Models.Entities
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApi.Models.Entities
 {
-    public class CreditcardEntity
+    public class CreditCardEntity
     {
+        [Required]
+        [Key]
         public int Id { get; set; }
+
+        [Required]
         public string CardNumber { get; set; }
+
+        [Required]
         public DateTime ExpirationTime { get; set; }
+
+        [Required]
         public string CVV { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(UserID))]
+        public string UserID { get; set; }
+
+        [Required]
+        public UserEntity User { get; set; }
 
     }
 }
